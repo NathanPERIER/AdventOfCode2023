254798398d7b7a0c4f112c630a4d8c382c269410
NathanPERIER <periernathan2000@gmail.com>
2023-12-05 00:20:37 +0100

    Day 3 - Part 2

--- src/day-03.cc
+++ src/day-03.cc
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 
 bool is_number(char c) {
@@ -8,11 +9,7 @@ bool is_number(char c) {
 }
 
 
-uint64_t extract_number(std::string& line, size_t pos) {
-    if(!is_number(line[pos])) {
-        return 0;
-    }
-
+std::pair<size_t,size_t> find_number_range(const std::string& line, size_t pos) {
     size_t begin = pos;
     while(begin > 0 && is_number(line[begin-1])) {
         begin--;
@@ -23,6 +20,23 @@ uint64_t extract_number(std::string& line, size_t pos) {
         end++;
     }
 
+    return { begin, end };
+}
+
+
+
+/*-----------------+
+|  First Part      |
++------------------*/
+
+
+uint64_t extract_number(std::string& line, size_t pos) {
+    if(!is_number(line[pos])) {
+        return 0;
+    }
+
+    const auto [begin, end] = find_number_range(line, pos);
+
     std::string repr = line.substr(begin, end - begin);
     for(size_t i = begin; i < end; i++) { // clear to avoid reading an integer several times
         line[i] = '.';
@@ -32,7 +46,7 @@ uint64_t extract_number(std::string& line, size_t pos) {
 }
 
 
-uint64_t process_lines(std::vector<std::string>& lines) {
+uint64_t process_lines_pt1(std::vector<std::string>& lines) {
     uint64_t res = 0;
 
     // Note : it is technically possible to do this while only storing
@@ -75,7 +89,82 @@ uint64_t process_lines(std::vector<std::string>& lines) {
 }
 
 
-int main() {
+
+/*-----------------+
+|  Second Part      |
++------------------*/
+
+
+uint64_t parse_number(const std::string& line, size_t pos, uint8_t& success_count) {
+    if(!is_number(line[pos])) {
+        return 1;
+    }
+
+    success_count++; // usually a bad idea but extremely convenient here
+    const auto [begin, end] = find_number_range(line, pos);
+
+    std::string repr = line.substr(begin, end - begin);
+    return std::stoull(repr);
+}
+
+
+uint64_t process_lines_pt2(std::vector<std::string>& lines) {
+    uint64_t res = 0;
+
+    for(size_t i = 0; i < lines.size(); i++) {
+        const std::string& line = lines[i];
+        for(size_t j = 0; j < line.size(); j++) {
+            if(line[j] != '*') {
+                continue;
+            }
+            uint8_t number_count = 0;
+            uint64_t ratio = 1;
+            // I'm sure there's a better way to do this...
+            if(i > 0) {
+                if(is_number(lines[i-1][j])) {
+                    ratio *= parse_number(lines[i-1], j, number_count);
+                } else {
+                    if(j > 0) {
+                        ratio *= parse_number(lines[i-1], j-1, number_count);
+                    }
+                    if(j < line.size() - 1) {
+                        ratio *= parse_number(lines[i-1], j+1, number_count);
+                    }
+                }
+            }
+            if(i < lines.size() - 1) {
+                if(is_number(lines[i+1][j])) {
+                    ratio *= parse_number(lines[i+1], j, number_count);
+                } else {
+                    if(j > 0) {
+                        ratio *= parse_number(lines[i+1], j-1, number_count);
+                    }
+                    if(j < line.size() - 1) {
+                        ratio *= parse_number(lines[i+1], j+1, number_count);
+                    }
+                }
+            }
+            if(j > 0) {
+                ratio *= parse_number(line, j-1, number_count);
+            }
+            if(j < line.size() - 1) {
+                ratio *= parse_number(line, j+1, number_count);
+            }
+            if(number_count == 2) {
+                res += ratio;
+            }
+        }
+    }
+
+    return res;
+}
+
+
+
+int main(int argc, char** argv) {
+
+    const bool is_part_2 = (argc >= 2 && std::string("--part2") == argv[1]);
+	const auto process_lines = is_part_2 ? process_lines_pt2 : process_lines_pt1;
 
     std::vector<std::string> lines;
 
@@ -92,9 +181,6 @@ int main() {
 	}
 
     uint64_t sum = process_lines(lines);
-    for(auto& line: lines) {
-        std::cout << line << std::endl;
-    }
     std::cout << "Sum of the part numbers : " << sum << std::endl;
 
     return 0;
