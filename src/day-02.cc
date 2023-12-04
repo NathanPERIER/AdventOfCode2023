3332c1393a05772952e53cf5ac76b9ec5d79f2c8
NathanPERIER <periernathan2000@gmail.com>
2023-12-04 14:37:22 +0100

    Day 2 - Part 2

--- src/day-02.cc
+++ src/day-02.cc
@@ -10,6 +10,10 @@ struct cubes {
     uint32_t red = 0;
     uint32_t green = 0;
     uint32_t blue = 0;
+
+    uint64_t power() {
+        return red * green * blue;
+    }
 };
 
 
@@ -20,8 +24,37 @@ bool is_possible(const std::vector<cubes>& game) {
     });
 }
 
+uint64_t part1(const std::pair<uint32_t,std::vector<cubes>>& game) {
+    if(is_possible<12, 13, 14>(game.second)) {
+        return game.first;
+    }
+    return 0;
+}
 
-int main() {
+
+uint64_t part2(const std::pair<uint32_t,std::vector<cubes>>& game) {
+    cubes max;
+    for(const cubes& cb: game.second) {
+        if(cb.red > max.red) {
+            max.red = cb.red;
+        }
+        if(cb.green > max.green) {
+            max.green = cb.green;
+        }
+        if(cb.blue > max.blue) {
+            max.blue = cb.blue;
+        }
+    }
+    std::cout << "Min for game " << game.first << " : " << max.red << " red, " << max.green << " green, " << max.blue << " blue, " << std::endl;
+    return max.power();
+}
+
+
+int main(int argc, char** argv) {
+
+    const bool is_part_2 = (argc >= 2 && std::string("--part2") == argv[1]);
+	const auto compute = is_part_2 ? part2 : part1;
+
     std::vector<std::pair<uint32_t,std::vector<cubes>>> games;
 
     while(true) {
@@ -37,9 +70,9 @@ int main() {
         const auto [_, num_repr] = split_once(pre, " ");
         const uint32_t game_num = std::stoul(std::string(num_repr)); // TODO better conversion
         std::vector<cubes> game;
-        for(std::string_view grab: split(list, "; ")) {
+        for(const std::string_view grab: split(list, "; ")) {
             cubes cb;
-            for(std::string_view nb_cubes: split(grab, ", ")) {
+            for(const std::string_view nb_cubes: split(grab, ", ")) {
                 auto [count_repr, colour] = split_once(nb_cubes, " ");
                 const uint32_t count = std::stoul(std::string(count_repr)); // TODO better conversion
                 if(colour == "red") {
@@ -56,10 +89,8 @@ int main() {
 	}
 
     uint64_t sum = 0;
-    for(auto& game: games) {
-        if(is_possible<12, 13, 14>(game.second)) {
-            sum += game.first;
-        }
+    for(const auto& game: games) {
+        sum += compute(game);
     }
     std::cout << "Sum of the game IDs : " << sum << std::endl;
 
